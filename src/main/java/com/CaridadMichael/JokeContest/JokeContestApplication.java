2078807aa5c33d6b51ddbcdddc9c814c3eac5c35
Michael Caridad <caridadmichael@gmail.com>
2022-01-20 04:24:55 -0500

    initial commit

---
+++ src/main/java/com/CaridadMichael/JokeContest/JokeContestApplication.java
@@ -0,0 +1,13 @@
+package com.CaridadMichael.JokeContest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeContestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeContestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CaridadMichael/JokeContest/JokeContestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CaridadMichael.JokeContest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeContestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
