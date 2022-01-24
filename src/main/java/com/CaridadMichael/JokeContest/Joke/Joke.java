80d292aa91c48a73ee6b42a52dd581551125e95c
Michael Caridad <caridadmichael@gmail.com>
2022-01-23 21:31:29 -0500

    change generated value to identity and added some more fields

--- src/main/java/com/CaridadMichael/JokeContest/Joke/Joke.java
+++ src/main/java/com/CaridadMichael/JokeContest/Joke/Joke.java
@@ -1,5 +1,80 @@
 package com.CaridadMichael.JokeContest.Joke;
 
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+
+
+@Entity
+@Table
 public class Joke {
+	@Id	
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="joke_id")
+	private long id;	
+	private String joke;
+	private int rating = 0;
+
+	
+	public Joke() {}
+
+	public Joke(long id,  String joke,int rating) {
+		this.id = id;	
+		this.joke = joke;	
+		this.rating = rating;
+	}
+	
+	public Joke( String author, String joke,  int rating) {	
+		this.joke = joke;
+	
+		this.rating = rating;
+	}
+
+	
+	public long getId() {
+		return id;
+	}
+
+	
+	public void setId(long id) {
+		this.id = id;
+	}	
+
+	
+	public String getJoke() {
+		return joke;
+	}
+
+	
+	public void setJoke(String joke) {
+		this.joke = joke;
+	}
+
+	
+
+
+	
+	public int getRating() {
+		return rating;
+	}
+
+	
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	@Override
+	public String toString() {
+		return "Joke [id=" + id + " joke=" + joke + ", rating=" + rating
+				+ "]";
+	}
+	
+	
 
 }
