86ae7a0944e47631ffbefdcf7a5ea8e67791938d
Michael Caridad <caridadmichael@gmail.com>
2022-01-23 21:30:20 -0500

    added logic for saving joke and upvote/downvote

--- src/main/java/com/CaridadMichael/JokeContest/Joke/JokeService.java
+++ src/main/java/com/CaridadMichael/JokeContest/Joke/JokeService.java
@@ -1,5 +1,35 @@
 package com.CaridadMichael.JokeContest.Joke;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JokeService {
+	
+	private final JokeRepository jokeRepo;
+
+	@Autowired
+	public JokeService(JokeRepository jokeRepo) {		
+		this.jokeRepo = jokeRepo;
+	}
+	
+
+	public Joke createJoke(Joke joke) {		 
+	     return jokeRepo.save(joke);
+	}
+	
+	public Joke upVote(Joke joke) {
+		Joke updateJoke = jokeRepo.findById(joke.getId()).get();
+		updateJoke.setJoke(updateJoke.getJoke());
+		updateJoke.setRating(updateJoke.getRating() + 1);
+		return jokeRepo.save(updateJoke);
+	}
+	
+	public Joke downVote(Joke joke) {
+		Joke updateJoke = jokeRepo.findById(joke.getId()).get();
+		updateJoke.setJoke(updateJoke.getJoke());
+		updateJoke.setRating(updateJoke.getRating() - 1);
+		return jokeRepo.save(updateJoke);
+	}
 
 }
